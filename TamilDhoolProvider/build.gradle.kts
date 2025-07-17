e7f0da00398733828054abfc28e9126e215d47bf
aravinddwara <149893688+aravinddwara@users.noreply.github.com>
2025-07-17 10:45:26 +0530

    Create build.gradle.kts

---
+++ TamilDhoolProvider/build.gradle.kts
@@ -0,0 +1,23 @@
+// use an integer for version numbers
+version = 1
+
+cloudstream {
+    // All of these properties are optional, you can safely remove any of them.
+
+    description = "Watch Tamil serials and shows from TamilDhool"
+    authors = listOf("YourName")
+
+    /**
+     * Status int as one of the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta-only
+     **/
+    status = 1 // Will be 3 if unspecified
+
+    tvTypes = listOf("TvSeries")
+    iconUrl = "https://www.google.com/s2/favicons?domain=tamildhool.net&sz=%size%"
+
+    isCrossPlatform = true
+}
