4956fa321552af3f4ec3eae1b0762a9b26180d7d
aravinddwara <149893688+aravinddwara@users.noreply.github.com>
2025-09-27 22:08:24 +0530

    Update ThiraiOneExtractor.kt

--- TamilDhoolProvider/src/main/kotlin/recloudstream/ThiraiOneExtractor.kt
+++ TamilDhoolProvider/src/main/kotlin/recloudstream/ThiraiOneExtractor.kt
@@ -2,7 +2,9 @@ package recloudstream
 
 import com.lagradost.cloudstream3.app
 import com.lagradost.cloudstream3.SubtitleFile
-import com.lagradost.cloudstream3.utils.*
+import com.lagradost.cloudstream3.utils.ExtractorApi
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.Qualities
 import org.jsoup.nodes.Document
 
 class ThiraiOneExtractor : ExtractorApi() {
