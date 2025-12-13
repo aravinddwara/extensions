0341b0c8d932016ecf71a06b7cd1eb1bdce87bce
aravinddwara <149893688+aravinddwara@users.noreply.github.com>
2025-12-13 15:21:14 +0530

    Update TamilDhoolProvider.kt

--- TamilDhoolProvider/src/main/kotlin/recloudstream/TamilDhoolProvider.kt
+++ TamilDhoolProvider/src/main/kotlin/recloudstream/TamilDhoolProvider.kt
@@ -236,14 +236,11 @@ class TamilDhoolProvider : MainAPI() {
                 val m3u8Url = m3u8Match.value
                 
                 callback.invoke(
-                    ExtractorLink(
+                    M3u8Helper.generateM3u8(
                         source = name,
-                        name = "Thrfive - HLS",
-                        url = m3u8Url,
-                        referer = "https://tamilbliss.com/",
-                        quality = Qualities.Unknown.value,
-                        isM3u8 = true
-                    )
+                        streamUrl = m3u8Url,
+                        referer = "https://tamilbliss.com/"
+                    ).first()
                 )
             } else {
                 // Method 2: Generic stream URL pattern (for mirrors)
@@ -254,14 +251,11 @@ class TamilDhoolProvider : MainAPI() {
                     val m3u8Url = genericMatch.value
                     
                     callback.invoke(
-                        ExtractorLink(
+                        M3u8Helper.generateM3u8(
                             source = name,
-                            name = "Thrfive - HLS",
-                            url = m3u8Url,
-                            referer = "https://tamilbliss.com/",
-                            quality = Qualities.Unknown.value,
-                            isM3u8 = true
-                        )
+                            streamUrl = m3u8Url,
+                            referer = "https://tamilbliss.com/"
+                        ).first()
                     )
                 }
             }
