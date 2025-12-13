88ff1b083babc5c9ba8c707bd172cca0b2eb4a16
aravinddwara <149893688+aravinddwara@users.noreply.github.com>
2025-12-13 15:16:08 +0530

    Update TamilDhoolProvider.kt

--- TamilDhoolProvider/src/main/kotlin/recloudstream/TamilDhoolProvider.kt
+++ TamilDhoolProvider/src/main/kotlin/recloudstream/TamilDhoolProvider.kt
@@ -236,15 +236,14 @@ class TamilDhoolProvider : MainAPI() {
                 val m3u8Url = m3u8Match.value
                 
                 callback.invoke(
-                    newExtractorLink(
+                    ExtractorLink(
                         source = name,
                         name = "Thrfive - HLS",
-                        url = m3u8Url
-                    ) {
-                        referer = "https://tamilbliss.com/"
-                        quality = Qualities.Unknown.value
+                        url = m3u8Url,
+                        referer = "https://tamilbliss.com/",
+                        quality = Qualities.Unknown.value,
                         isM3u8 = true
-                    }
+                    )
                 )
             } else {
                 // Method 2: Generic stream URL pattern (for mirrors)
@@ -255,15 +254,14 @@ class TamilDhoolProvider : MainAPI() {
                     val m3u8Url = genericMatch.value
                     
                     callback.invoke(
-                        newExtractorLink(
+                        ExtractorLink(
                             source = name,
                             name = "Thrfive - HLS",
-                            url = m3u8Url
-                        ) {
-                            referer = "https://tamilbliss.com/"
-                            quality = Qualities.Unknown.value
+                            url = m3u8Url,
+                            referer = "https://tamilbliss.com/",
+                            quality = Qualities.Unknown.value,
                             isM3u8 = true
-                        }
+                        )
                     )
                 }
             }
